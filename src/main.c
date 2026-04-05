0c369f9fa5eaa3fb9a3b368efea36c1a54a1319c
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-05 03:56:04 +0200

    envp

--- src/main.c
+++ src/main.c
@@ -1,13 +1,18 @@
 #include "minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+int main(int argc, char **argv, char **envp)
 {
-	t_shell	shell;
+	t_shell shell;
 
 	(void)argc;
 	(void)argv;
+	int i = 0;
+	while (envp[i])
+	{
+		printf("%s\n", envp[i]);
+		i++;
+	}
 	init_shell(&shell, envp);
 	run_shell(&shell);
 	return (shell.last_exit_status);
 }
-
