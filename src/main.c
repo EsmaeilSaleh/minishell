7c930d2f8ec13e2f2895dc6ec0ec49ca1d6795ce
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-05 04:04:23 +0200

    envp

--- src/main.c
+++ src/main.c
@@ -1,5 +1,19 @@
 #include "minishell.h"
 
+void *get_path_from_envp(char **envp)
+{
+	int i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (strncmp(envp[i], "PATH=", 5) == 0)
+			printf("%s\n", envp[i]);
+		i++;
+	}
+	return (NULL);
+}
+
 int main(int argc, char **argv, char **envp)
 {
 	t_shell shell;
@@ -9,6 +23,7 @@ int main(int argc, char **argv, char **envp)
 	init_shell(&shell, envp);
 	run_shell(&shell);
 	int i = 0;
+	get_path_from_envp(envp);
 	while (envp[i])
 	{
 		printf("%s\n", envp[i]);
