b43a0aae259ae918653766119c3698e838ec3b58
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-06 20:40:44 +0200

    string helpers

--- src/lexer/token_print.c
+++ src/lexer/token_print.c
@@ -2,6 +2,19 @@
 
 static char *token_type_name(t_token_type type)
 {
+    if (type == TOK_WORD)
+        return ("WORD");
+    if (type == TOK_PIPE)
+        return ("PIPE");
+    if (type == TOK_REDIR_IN)
+        return ("REDIR_IN");
+    if (type == TOK_REDIR_OUT)
+        return ("REDIR_OUT");
+    if (type == TOK_APPEND)
+        return ("APPEND");
+    if (type == TOK_HEREDOC)
+        return ("HEREDOC");
+    return ("UNKNOWN");
 }
 
 void print_tokens(t_token *tokens)
