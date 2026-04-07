a59590397677dc12619cd8947a865c63d81f06d4
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-07 07:32:21 +0200

    syntax check added

--- src/syntax/syntax_check.c
+++ src/syntax/syntax_check.c
@@ -1,49 +1,48 @@
 #include "minishell.h"
 
-static int	check_pipe_syntax(t_token *tokens)
+static int check_pipe_syntax(t_token *tokens)
 {
-	if (is_pipe_token(tokens))
-		return (0);
-	while (tokens)
-	{
-		if (is_pipe_tokens(tokens))
-		{
-			if (tokens->next == NULL)
-				return (0);
-			if (is_pipe_token(tokens->next))
-				return (0);
-			if (is_redir_token(tokens->next))
-				return (0);
-		}
-		tokens = tokens->next;
-	}
-	return (1);
-
+    if (is_pipe_token(tokens))
+        return (0);
+    while (tokens)
+    {
+        if (is_pipe_token(tokens))
+        {
+            if (tokens->next == NULL)
+                return (0);
+            if (is_pipe_token(tokens->next))
+                return (0);
+            if (is_redir_token(tokens->next))
+                return (0);
+        }
+        tokens = tokens->next;
+    }
+    return (1);
 }
 
-static int	check_redir_syntax(t_token *tokens)
+static int check_redir_syntax(t_token *tokens)
 {
-	while (tokens)
-	{
-		if (is_redir_token(tokens))
-		{
-			if (tokens->next == NULL)
-				return (0);
-			if (tokens->next->type != TOK_WORD)
-				return (0);
-		}
-		tokens = tokens->next;
-	}
-	return (1);
+    while (tokens)
+    {
+        if (is_redir_token(tokens))
+        {
+            if (tokens->next == NULL)
+                return (0);
+            if (tokens->next->type != TOK_WORD)
+                return (0);
+        }
+        tokens = tokens->next;
+    }
+    return (1);
 }
 
-int	syntax_check(t_token *token)
+int syntax_check(t_token *token)
 {
-	if (tokens == NULL)
-		return (1);
-	if (!check_pipe_syntax(tokens))
-		return (syntax_error());
-	if (!check_redir_syntax(tokens))
-		return (syntax_error());
-	return (1);
+    if (token == NULL)
+        return (1);
+    if (!check_pipe_syntax(token))
+        return (syntax_error());
+    if (!check_redir_syntax(token))
+        return (syntax_error());
+    return (1);
 }
