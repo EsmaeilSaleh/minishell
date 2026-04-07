c89dd4836e30b61d696e30876055050827669c01
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-07 07:20:56 +0200

    update

--- src/syntax/syntax_utils.c
+++ src/syntax/syntax_utils.c
@@ -10,3 +10,9 @@ int	is_redir_token(t_token *token)
 	t_token_type type = token->type;
 	return (type == TOK_REDIR_IN || type == TOK_REDIR_OUT || type == TOK_APPEND || type == TOK_HEREDOC);
 }
+
+int	syntax_error(void)
+{
+	printf("syntax error near unexpected token\n");
+	return (0);
+}
