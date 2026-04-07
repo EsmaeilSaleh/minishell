c7787b6a06077e70466b734ed6b42337fa1fbca0
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-07 07:09:08 +0200

    update

---
+++ src/syntax/syntax_utils.c
@@ -0,0 +1,12 @@
+#include "minishell.h"
+
+int is_pipe_token(t_token *token)
+{
+	return (token->type == TOK_PIPE);
+}
+
+int	is_redir_token(t_token *token)
+{
+	t_token_type type = token->type;
+	return (type == TOK_REDIR_IN || type == TOK_REDIR_OUT || type == TOK_APPEND || type == TOK_HEREDOC);
+}
