eeada5e98fab28e84e04f8c8194ac34c224b2a95
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-07 07:14:08 +0200

    update

--- src/syntax/syntax_check.c
+++ src/syntax/syntax_check.c
@@ -1,1 +1,32 @@
 #include "minishell.h"
+
+static int	check_pipe_syntax(t_token *tokens)
+{
+	if (is_pipe_token(tokens))
+		return (0);
+	while (tokens)
+	{
+		if (is_pipe_tokens(tokens))
+		{
+			if (tokens->next == NULL)
+				return (0);
+			if (is_pipe_token(tokens->next))
+				return (0);
+			if (is_redir_token(tokens->next))
+				return (0);
+		}
+		tokens = tokens->next;
+	}
+	return (1);
+
+}
+
+static int	check_redir_syntax(t_token *tokens)
+{
+
+}
+
+int	syntax_check(t_token *token)
+{
+
+}
