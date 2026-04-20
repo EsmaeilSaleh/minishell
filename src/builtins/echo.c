0ee7df8d9ddc5083fedb171263d8ee3ef0c41582
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-20 20:14:39 +0200

    builtins

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -5,12 +5,12 @@ int ft_echo(char **argv)
     int i;
     int newline;
 
-    i = 0;
+    i = 1;
     newline = 1;
     if (argv[1] && ft_strcmp(argv[1], "-n") == 0)
     {
         newline = 0;
-        i = 0;
+        i = 2;
     }
     while (argv[i])
     {
