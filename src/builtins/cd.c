715bbe18cfd3c55ea55b76a59f223ee043fbd2d6
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-20 20:41:30 +0200

    cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -2,7 +2,16 @@
 
 int ft_cd(char **argv, t_shell *shell)
 {
-    (void)argv;
     (void)shell;
+    if (argv[1] == NULL)
+    {
+        printf("cd: missing argument\n");
+        return (1);
+    }
+    if (chdir(argv[1]) != 0)
+    {
+        perror("cd");
+        return (1);
+    }
     return (0);
 }
