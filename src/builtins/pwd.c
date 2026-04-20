1b3bb72459ad5157b5771ee5733430e05526ce57
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-20 20:29:53 +0200

    pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -2,5 +2,13 @@
 
 int ft_pwd(void)
 {
+    char buffer[1024];
+
+    if (getcwd(buffer, sizeof(buffer)) == NULL)
+    {
+        perror("pwd");
+        return (1);
+    }
+    printf("%s\n", buffer);
     return (0);
 }
