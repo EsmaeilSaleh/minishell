74769e6bcc867bcaf768ff7213138ce017b81b73
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-21 08:39:11 +0200

    ft_atoi

--- src/utils/strings.c
+++ src/utils/strings.c
@@ -74,3 +74,26 @@ int ft_strcmp(const char *s1, const char *s2)
     }
     return (0);
 }
+
+int ft_atoi(const char *s)
+{
+    int sign;
+    long result;
+
+    sign = 1;
+    result = 0;
+    while (*s == ' ' || (*s >= '\t' && *s <= '\r'))
+        s++;
+    if (*s == '-' || *s == '+')
+    {
+        if (*s == '-')
+            sign = -1;
+        s++;
+    }
+    while (*s >= '0' && *s <= '9')
+    {
+        result = result * 10 + (*s - '0');
+        s++;
+    }
+    return ((int)(result * sign));
+}
