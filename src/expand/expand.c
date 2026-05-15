89b6445bcd3a6aea378c4fd2c574b091d31dc0c9
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-05-15 18:43:38 +0200

    quote

---
+++ src/expand/expand.c
@@ -0,0 +1,11 @@
+#include "minishell.h"
+
+char *expand_one_word(char *word, t_shell *shell)
+{
+    return (NULL);
+}
+
+void expand_cmds(t_cmd *cmds, t_shell *shell)
+{
+    return;
+}
--- src/lexer/lexer_utils.c
+++ src/lexer/lexer_utils.c
@@ -10,12 +10,28 @@ int is_operator_char(char c)
     return (c == '|' || c == '>' || c == '<');
 }
 
+static void skip_quoted_section(char *line, int *i, char quote)
+{
+    (*i)++;
+    while (line[*i] && line[*i] != quote)
+        (*i)++;
+    if (line[*i] == quote)
+        (*i)++;
+}
+
 char *extract_word(char *line, int *i)
 {
     int start;
 
     start = *i;
-    while (line[*i] && !is_space(line[*i]) && !is_operator_char(line[*i]))
-        (*i)++;
+    while (line[*i])
+    {
+        if (line[*i] == '\'' || line[*i] == '"')
+            skip_quoted_section(line, i, line[*i]);
+        else if (is_space(line[*i]) || is_operator_char(line[*i]))
+            break;
+        else
+            (*i)++;
+    }
     return (ft_substr(line, start, *i - start));
 }
