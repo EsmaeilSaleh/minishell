a6ed47e4d917a7f64a9666012ad972097c159d3f
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-05-20 06:54:00 +0200

    itoa

--- include/minishell.h
+++ include/minishell.h
@@ -53,6 +53,7 @@ char *ft_strdup(const char *s);
 int ft_strlen(const char *s);
 int ft_strcmp(const char *s1, const char *s2);
 int ft_atoi(const char *s);
+char *ft_itoa(int n);
 
 void init_shell(t_shell *shell, char **envp);
 void run_shell(t_shell *shell);
--- src/utils/strings.c
+++ src/utils/strings.c
@@ -97,3 +97,35 @@ int ft_atoi(const char *s)
     }
     return ((int)(result * sign));
 }
+
+char *ft_itoa(int n)
+{
+    char *str;
+    long nb;
+    int len;
+    nb = n;
+    len = 1;
+    if (nb < 0)
+    {
+        nb = -nb;
+        len++;
+    }
+    long temp = nb;
+    while (temp >= 10)
+    {
+        temp /= 10;
+        len++;
+    }
+    str = (char *)malloc(sizeof(char) * (len + 1));
+    if (!str)
+        return (NULL);
+    str[len] = '\0';
+    while (len--)
+    {
+        str[len] = (nb % 10) + '0';
+        nb /= 10;
+    }
+    if (n < 0)
+        str[0] = '-';
+    return (str);
+}
