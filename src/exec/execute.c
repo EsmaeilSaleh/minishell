de2975a3f348ac48c4c12c893e60e5b3d93ff335
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-05-25 11:46:36 +0200

    heredoc

--- src/exec/execute.c
+++ src/exec/execute.c
@@ -4,6 +4,11 @@ int execute_cmds(t_cmd *cmds, t_shell *shell)
 {
     if (cmds == NULL)
         return (0);
+    if (prepare_heredocs(cmds) != 0)
+    {
+        shell->last_exit_status = 1;
+        return (1);
+    }
     if (cmds->next == NULL)
         return (execute_single(cmds, shell));
     return (execute_pipeline(cmds, shell));
