10fc8af042c2d6ac9f91ee5d0bc78751c2310f4f
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-05-25 11:49:51 +0200

    heredoc

--- include/minishell.h
+++ include/minishell.h
@@ -121,5 +121,5 @@ void exec_child_process(t_cmd *cmd, t_shell *shell, int prev_fd, int pipefd[2], 
 void expand_cmds(t_cmd *cmds, t_shell *shell);
 char *expand_one_word(char *word, t_shell *shell);
 
-int prepare_heredoc(char *delimiter);
+int prepare_heredoc(t_cmd *cmds);
 #endif
