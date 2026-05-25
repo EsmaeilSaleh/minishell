b146de0f430611754137e530deef3fbc40e09add
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-05-25 16:04:00 +0200

    heredoc

--- src/heredoc/heredoc.c
+++ src/heredoc/heredoc.c
@@ -1,20 +1,4 @@
 #include "minishell.h"
-void setup_signals(void)
-{
-    rl_done = 0;
-    g_signal_status = 0;
-    set_signal_handler(SIGINT, sigint_handler);
-    set_signal_handler(SIGQUIT, SIG_IGN);
-}
-
-void setup_heredoc_signals(void)
-{
-    rl_done = 0;
-    g_signal_status = 0;
-    set_signal_handler(SIGINT, heredoc_sigint_handler);
-    set_signal_handler(SIGQUIT, SIG_IGN);
-}
-#include "minishell.h"
 
 int prepare_heredoc(char *delimiter)
 {
