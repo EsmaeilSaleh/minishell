47531e7ed5ce7a30fa6561a5defeaacd90970169
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-05-25 15:29:01 +0200

    heredoc

--- src/exec/execute.c
+++ src/exec/execute.c
@@ -1,5 +1,23 @@
 #include "minishell.h"
 
+void heredoc_sigint_handler(int signum)
+{
+    (void)signum;
+    g_signal_status = SIGINT;
+    write(1, "\n", 1);
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_done = 1;
+}
+
+void setup_signals(void)
+{
+    rl_done = 0;
+    g_signal_status = 0;
+    signal(SIGINT, sigint_handler);
+    signal(SIGQUIT, SIG_IGN);
+}
+
 int execute_cmds(t_cmd *cmds, t_shell *shell)
 {
     if (cmds == NULL)
