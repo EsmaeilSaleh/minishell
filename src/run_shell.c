65f0beae630dff841841b4efe9c3f2dfacfc340e
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-05-26 09:15:52 +0200

    fix

--- src/run_shell.c
+++ src/run_shell.c
@@ -1,5 +1,56 @@
 #include "minishell.h"
 
+static char	*read_non_interactive_line(void)
+{
+	char	*line;
+	char	c;
+	int		len;
+	int		capacity;
+	int		bytes_read;
+	char	*new_line;
+	int		i;
+
+	line = malloc(128);
+	if (line == NULL)
+		return (NULL);
+	len = 0;
+	capacity = 128;
+	while (1)
+	{
+		bytes_read = read(STDIN_FILENO, &c, 1);
+		if (bytes_read <= 0)
+			break ;
+		if (c == '\n')
+			break ;
+		if (len + 1 >= capacity)
+		{
+			new_line = malloc(capacity * 2);
+			if (new_line == NULL)
+			{
+				free(line);
+				return (NULL);
+			}
+			i = 0;
+			while (i < len)
+			{
+				new_line[i] = line[i];
+				i++;
+			}
+			free(line);
+			line = new_line;
+			capacity *= 2;
+		}
+		line[len++] = c;
+	}
+	if (bytes_read <= 0 && len == 0)
+	{
+		free(line);
+		return (NULL);
+	}
+	line[len] = '\0';
+	return (line);
+}
+
 static void handle_input(t_shell *shell, char *line)
 {
 	t_token *tokens;
@@ -44,7 +95,7 @@ void run_shell(t_shell *shell)
 		if (interactive)
 			line = readline("minishell$ ");
 		else
-			line = readline("");
+			line = read_non_interactive_line();
 		if (line == NULL)
 		{
 			if (interactive)
