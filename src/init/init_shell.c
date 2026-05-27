fa50b10cac5320cee1e8df124955b29719582274
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-05-27 14:42:56 +0200

    norminette init

--- src/init/init_shell.c
+++ src/init/init_shell.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_shell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esmaeil <esmaeil@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 00:00:00 by esmaeil           #+#    #+#             */
+/*   Updated: 2026/05/27 00:00:00 by esmaeil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-char **copy_envp(char **envp)
+char	**copy_envp(char **envp)
 {
-	char **new_env;
-	int count;
-	int i;
+	char	**new_env;
+	int		count;
+	int		i;
 
 	count = env_count(envp);
 	new_env = malloc(sizeof(char *) * (count + 1));
@@ -20,38 +32,12 @@ char **copy_envp(char **envp)
 	return (new_env);
 }
 
-static void	set_shlvl(t_shell *shell)
+static void	add_env_entry(t_shell *shell, char *entry)
 {
-	char	*val;
-	int		lvl;
-	char	*new_val;
-	char	*entry;
-	int		idx;
-	int		count;
 	char	**new_env;
+	int		count;
 	int		i;
 
-	val = get_env_value(shell->envp, "SHLVL");
-	if (val)
-		lvl = ft_atoi(val) + 1;
-	else
-		lvl = 1;
-	if (lvl < 0)
-		lvl = 0;
-	new_val = ft_itoa(lvl);
-	if (!new_val)
-		return ;
-	entry = ft_strjoin("SHLVL=", new_val);
-	free(new_val);
-	if (!entry)
-		return ;
-	idx = env_find_index(shell->envp, "SHLVL");
-	if (idx != -1)
-	{
-		free(shell->envp[idx]);
-		shell->envp[idx] = entry;
-		return ;
-	}
 	count = env_count(shell->envp);
 	new_env = malloc(sizeof(char *) * (count + 2));
 	if (!new_env)
@@ -71,51 +57,58 @@ static void	set_shlvl(t_shell *shell)
 	shell->envp = new_env;
 }
 
-static void	init_oldpwd(t_shell *shell)
+static void	set_env_entry(t_shell *shell, char *entry, char *key)
 {
-	int		idx;
-	int		count;
-	char	**new_env;
-	int		i;
-	char	*entry;
+	int	idx;
 
-	idx = env_find_index(shell->envp, "OLDPWD");
+	idx = env_find_index(shell->envp, key);
 	if (idx != -1)
 	{
-		if (shell->envp[idx][6] == '=')
-		{
-			free(shell->envp[idx]);
-			shell->envp[idx] = ft_strdup("OLDPWD");
-		}
+		free(shell->envp[idx]);
+		shell->envp[idx] = entry;
 		return ;
 	}
-	count = env_count(shell->envp);
-	new_env = malloc(sizeof(char *) * (count + 2));
-	if (!new_env)
-		return ;
-	i = 0;
-	while (i < count)
-	{
-		new_env[i] = shell->envp[i];
-		i++;
-	}
-	entry = ft_strdup("OLDPWD");
-	if (!entry)
-	{
-		free(new_env);
-		return ;
-	}
-	new_env[count] = entry;
-	new_env[count + 1] = NULL;
-	free(shell->envp);
-	shell->envp = new_env;
+	add_env_entry(shell, entry);
 }
 
-void init_shell(t_shell *shell, char **envp)
+static void	set_shlvl(t_shell *shell)
 {
+	char	*val;
+	char	*new_val;
+	char	*entry;
+	int		lvl;
+
+	val = get_env_value(shell->envp, "SHLVL");
+	if (val)
+		lvl = ft_atoi(val) + 1;
+	else
+		lvl = 1;
+	if (lvl < 0)
+		lvl = 0;
+	new_val = ft_itoa(lvl);
+	if (!new_val)
+		return ;
+	entry = ft_strjoin("SHLVL=", new_val);
+	free(new_val);
+	if (entry)
+		set_env_entry(shell, entry, "SHLVL");
+}
+
+void	init_shell(t_shell *shell, char **envp)
+{
+	int		idx;
+	char	*entry;
+
 	shell->envp = copy_envp(envp);
 	shell->last_exit_status = 0;
 	shell->running = 1;
 	set_shlvl(shell);
-	init_oldpwd(shell);
+	idx = env_find_index(shell->envp, "OLDPWD");
+	if (idx == -1 || shell->envp[idx][6] == '=')
+	{
+		entry = ft_strdup("OLDPWD");
+		if (!entry)
+			return ;
+		set_env_entry(shell, entry, "OLDPWD");
+	}
 }
