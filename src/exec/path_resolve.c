9a36ec05b0b79ee497ce540edb553a8c7d6003de
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4a13c8.42berlin.de>
2026-05-27 21:54:52 +0200

    Some changes for functionality - feel free to change back, Replaced strlen with ft_strlen, Replaced size_t with int, Fixed bug: ft_split now called AFTER PATH fallback, Split long path string across two lines

--- src/exec/path_resolve.c
+++ src/exec/path_resolve.c
@@ -1,6 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path_resolve.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkpg-md- <dkpg-md-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 00:17:23 by dkpg-md-          #+#    #+#             */
+/*   Updated: 2026/05/27 21:20:07 by dkpg-md-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
-#include <string.h>
-#include <sys/stat.h>
 
 static int	is_directory(char *path)
 {
@@ -11,76 +21,85 @@ static int	is_directory(char *path)
 	return (S_ISDIR(st.st_mode));
 }
 
-int has_slash(char *cmd)
+int	has_slash(char *cmd)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (cmd[i])
-    {
-        if (cmd[i] == '/')
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (cmd[i])
+	{
+		if (cmd[i] == '/')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-char *get_env_value(char **envp, const char *name)
+char	*get_env_value(char **envp, const char *name)
 {
-    int i;
-    size_t len;
+	int	i;
+	int	len;
 
-    i = 0;
-    len = strlen(name);
-    while (envp[i])
-    {
-        if (ft_strncmp(envp[i], name, len) == 0 && envp[i][len] == '=')
-            return (envp[i] + len + 1);
-        i++;
-    }
-    return (NULL);
+	i = 0;
+	len = ft_strlen(name);
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], name, len) == 0 && envp[i][len] == '=')
+			return (envp[i] + len + 1);
+		i++;
+	}
+	return (NULL);
 }
-char *resolve_command_path(char *cmd_name, char **envp)
-{
-    char *path_value;
-    char **dirs;
-    char *full_path;
-    char *found_noexec;
-    int i;
 
-    if (!cmd_name || !*cmd_name)
-        return (NULL);
-    if (has_slash(cmd_name))
-    {
-        if (access(cmd_name, F_OK) == 0)
-            return (ft_strdup(cmd_name));
-        return (NULL);
-    }
-    if (ft_strcmp(cmd_name, ".") == 0 || ft_strcmp(cmd_name, "..") == 0)
-        return (NULL);
-    path_value = get_env_value(envp, "PATH");
-    if (!path_value)
-        path_value = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
-    dirs = ft_split(path_value, ':');
-    if (!dirs)
-        return (NULL);
-    found_noexec = NULL;
-    i = 0;
-    while (dirs[i])
-    {
-        full_path = ft_join_path(dirs[i], cmd_name);
-        if (full_path && access(full_path, X_OK) == 0 && !is_directory(full_path))
-        {
-            free(found_noexec);
-            free_split(dirs);
-            return (full_path);
-        }
-        if (full_path && access(full_path, F_OK) == 0 && !is_directory(full_path)
-            && !found_noexec)
-            found_noexec = ft_strdup(full_path);
-        free(full_path);
-        i++;
-    }
-    free_split(dirs);
-    return (found_noexec);
+static char	*search_in_dirs(char **dirs, char *cmd_name)
+{
+	char	*full_path;
+	char	*found_noexec;
+	int		i;
+
+	i = 0;
+	found_noexec = NULL;
+	while (dirs[i])
+	{
+		full_path = ft_join_path(dirs[i], cmd_name);
+		if (full_path && access(full_path, X_OK) == 0
+			&& !is_directory(full_path))
+		{
+			free(found_noexec);
+			free_split(dirs);
+			return (full_path);
+		}
+		if (full_path && access(full_path, F_OK) == 0
+			&& !is_directory(full_path) && !found_noexec)
+			found_noexec = ft_strdup(full_path);
+		free(full_path);
+		i++;
+	}
+	free_split(dirs);
+	return (found_noexec);
+}
+
+char	*resolve_command_path(char *cmd_name, char **envp)
+{
+	char	*path_value;
+	char	**dirs;
+
+	if (!cmd_name || !*cmd_name)
+		return (NULL);
+	if (has_slash(cmd_name))
+	{
+		if (access(cmd_name, F_OK) == 0)
+			return (ft_strdup(cmd_name));
+		return (NULL);
+	}
+	if (ft_strcmp(cmd_name, ".") == 0 || ft_strcmp(cmd_name, "..") == 0)
+		return (NULL);
+	path_value = get_env_value(envp, "PATH");
+	if (!path_value)
+		path_value = "/usr/local/sbin:/usr/local/bin:/usr/sbin"
+			":/usr/bin:/sbin:/bin";
+	dirs = ft_split(path_value, ':');
+	if (!dirs)
+		return (NULL);
+	return (search_in_dirs(dirs, cmd_name));
 }
