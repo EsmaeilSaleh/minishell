45139c57984ada1c89e20444825074b5dd3a6eb5
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4a13c8.42berlin.de>
2026-05-27 21:48:34 +0200

    Added a signal function for child processes

--- src/signals/signals_utils.c
+++ src/signals/signals_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   signals_2.c                                        :+:      :+:    :+:   */
+/*   signals_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dkpg-md- <dkpg-md-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 16:15:04 by dkpg-md-          #+#    #+#             */
-/*   Updated: 2026/05/27 16:16:50 by dkpg-md-         ###   ########.fr       */
+/*   Updated: 2026/05/27 20:16:28 by dkpg-md-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,10 @@ void	setup_heredoc_signals(void)
 	g_signal_status = 0;
 	set_signal_handler(SIGINT, heredoc_sigint_handler);
 	set_signal_handler(SIGQUIT, SIG_IGN);
+}
+
+void	setup_child_signals(void)
+{
+	set_signal_handler(SIGINT, SIG_DFL);
+	set_signal_handler(SIGQUIT, SIG_DFL);
 }
