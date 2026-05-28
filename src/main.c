e4968b53f3d5b7fc98df7c8032cfcc9d65091ba6
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4a14c10.42berlin.de>
2026-05-28 19:45:42 +0200

    Norminetted

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkpg-md- <dkpg-md-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 00:17:23 by dkpg-md-          #+#    #+#             */
+/*   Updated: 2026/05/28 18:27:43 by dkpg-md-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-	t_shell shell;
+	t_shell	shell;
 
 	(void)argc;
 	(void)argv;
