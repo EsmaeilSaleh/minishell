0f63f12f037aa6a1772fe6a6f4af44bdda16f105
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4a14c10.42berlin.de>
2026-05-28 19:43:51 +0200

    Norminetted

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -1,15 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkpg-md- <dkpg-md-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 00:17:23 by dkpg-md-          #+#    #+#             */
+/*   Updated: 2026/05/28 17:52:16 by dkpg-md-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int ft_env(char **envp)
+int	ft_env(char **envp)
 {
-    int i;
-    if (envp == NULL)
-        return (0);
-    i = 0;
-    while (envp[i])
-    {
-        printf("%s\n", envp[i]);
-        i++;
-    }
-    return (0);
+	int	i;
+
+	if (envp == NULL)
+		return (0);
+	i = 0;
+	while (envp[i])
+	{
+		printf("%s\n", envp[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	env_find_index(char **envp, char *key)
+{
+	int	i;
+	int	key_len;
+
+	i = 0;
+	key_len = ft_strlen(key);
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], key, key_len) == 0
+			&& envp[i][key_len] == '=')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	env_count(char **envp)
+{
+	int	i;
+
+	i = 0;
+	if (!envp)
+		return (0);
+	while (envp[i])
+		i++;
+	return (i);
 }
