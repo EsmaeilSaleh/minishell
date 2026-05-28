f91e520a6d68eb24ee60af5a74e63b36a09a457e
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4a14c10.42berlin.de>
2026-05-28 19:42:32 +0200

    Updated minishell and norminetted

--- include/minishell.h
+++ include/minishell.h
@@ -1,22 +1,34 @@
-#ifndef MINISHELL_H
-#define MINISHELL_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkpg-md- <dkpg-md-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 00:17:23 by dkpg-md-          #+#    #+#             */
+/*   Updated: 2026/05/28 18:45:18 by dkpg-md-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <signal.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <sys/wait.h>
-#include <fcntl.h>
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <sys/stat.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/wait.h>
+# include <fcntl.h>
 
 typedef struct s_shell
 {
-	char **envp;
-	int last_exit_status;
-	int running;
-} t_shell;
+	char				**envp;
+	int					last_exit_status;
+	int					running;
+}	t_shell;
 
 typedef enum e_token_type
 {
@@ -26,32 +38,32 @@ typedef enum e_token_type
 	TOK_REDIR_OUT,
 	TOK_APPEND,
 	TOK_HEREDOC
-} t_token_type;
+}	t_token_type;
 
 typedef struct s_token
 {
-	char *value;
-	t_token_type type;
-	int fd;
-	struct s_token *next;
-} t_token;
+	char			*value;
+	t_token_type	type;
+	int				fd;
+	struct s_token	*next;
+}	t_token;
 
 typedef struct s_redir
 {
-	t_token_type type;
-	char *target;
-	int heredoc_fd;
-	int expand_body;
-	int fd;
-	struct s_redir *next;
-} t_redir;
+	t_token_type	type;
+	char			*target;
+	int				heredoc_fd;
+	int				expand_body;
+	int				fd;
+	struct s_redir	*next;
+}	t_redir;
 
 typedef struct s_cmd
 {
-	char **argv;
-	t_redir *redirs;
-	struct s_cmd *next;
-} t_cmd;
+	char			**argv;
+	t_redir			*redirs;
+	struct s_cmd	*next;
+}	t_cmd;
 
 typedef struct s_pipe_info
 {
@@ -61,92 +73,97 @@ typedef struct s_pipe_info
 	pid_t	last_pid;
 }	t_pipe_info;
 
-char *ft_substr(const char *s, int start, int len);
-char *ft_strdup(const char *s);
-int ft_strlen(const char *s);
-int ft_strcmp(const char *s1, const char *s2);
-char *ft_strjoin(const char *s1, const char *s2);
-int ft_atoi(const char *s);
-char *ft_itoa(int n);
+char		*ft_substr(const char *s, int start, int len);
+char		*ft_strdup(const char *s);
+int			ft_strlen(const char *s);
+int			ft_strcmp(const char *s1, const char *s2);
+char		*ft_strjoin(const char *s1, const char *s2);
+int			ft_atoi(const char *s);
+char		*ft_itoa(int n);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+char		*ft_join_path(char *dir, char *cmd);
+char		**ft_split(const char *s, char c);
 
-void init_shell(t_shell *shell, char **envp);
-void run_shell(t_shell *shell);
-void setup_signals(void);
+void		init_shell(t_shell *shell, char **envp);
+void		run_shell(t_shell *shell);
+void		setup_signals(void);
+void		setup_heredoc_signals(void);
+void		sigint_handler(int signum);
+void		heredoc_sigint_handler(int signum);
+void		set_signal_handler(int signo, void (*handler)(int));
 
-t_token *lexer(char *line);
-void print_tokens(t_token *tokens);
-void free_tokens(t_token *tokens);
+t_token		*lexer(char *line);
+void		print_tokens(t_token *tokens);
+void		free_tokens(t_token *tokens);
+t_token		*new_token(char *value, t_token_type type);
+void		add_token_back(t_token **list, t_token *new_node);
+int			is_space(char c);
+int			is_operator_char(char c);
+char		*extract_word(char *line, int *i);
+int			has_unclosed_quotes(char *line);
+int			find_semi_outside_quotes(char *line);
 
-t_token *new_token(char *value, t_token_type type);
-void add_token_back(t_token **list, t_token *new_node);
-int is_space(char c);
-int is_operator_char(char c);
-char *extract_word(char *line, int *i);
+int			is_pipe_token(t_token *token);
+int			is_redir_token(t_token *token);
+int			syntax_check(t_token *token);
+int			syntax_error(void);
 
-int is_pipe_token(t_token *token);
-int is_redir_token(t_token *token);
-int syntax_check(t_token *token);
-int syntax_error(void);
+t_cmd		*parser(t_token *token);
+t_cmd		*parse_command(t_token *tokens);
+int			count_cmd_args(t_token *tokens);
+t_redir		*new_redir(t_token_type type, char *target);
+void		add_redir_back(t_redir **list, t_redir *new_node);
+void		free_cmds(t_cmd *cmds);
+void		free_split(char **arr);
+void		free_redirs(t_redir *redirs);
+t_cmd		*new_cmd(char **argv, t_redir *redirs);
+void		add_cmd_back(t_cmd **list, t_cmd *new_node);
+void		print_cmds(t_cmd *cmds);
 
-t_cmd *parser(t_token *token);
-t_cmd *parse_command(t_token *tokens);
-int count_cmd_args(t_token *tokens);
+char		*get_env_value(char **envp, const char *name);
+int			has_slash(char *cmd);
+int			env_count(char **envp);
+int			env_find_index(char **envp, char *key);
 
-t_redir *new_redir(t_token_type type, char *target);
-void add_redir_back(t_redir **list, t_redir *new_node);
+int			handle_redir_only(t_cmd *cmd, t_shell *shell);
+int			handle_builtin(t_cmd *cmd, t_shell *shell);
+void		handle_child(t_cmd *cmd, t_shell *shell, char *path);
+int			handle_parent(int status, t_shell *shell);
+int			execute_cmds(t_cmd *cmds, t_shell *shell);
+int			execute_single(t_cmd *cmd, t_shell *shell);
+int			execute_pipeline(t_cmd *cmds, t_shell *shell);
+char		*resolve_command_path(char *cmd_name, char **envp);
+int			apply_redirs(t_redir *redirs);
+void		restore_stdio(int stdin_backup, int stdout_backup);
+void		exec_child_process(t_cmd *cmd, t_shell *shell, t_pipe_info *info);
+void		set_underscore(t_shell *shell, char *path);
 
-void free_cmds(t_cmd *cmds);
-void free_split(char **arr);
-void free_redirs(t_redir *redirs);
-t_cmd *new_cmd(char **argv, t_redir *redirs);
-void add_cmd_back(t_cmd **list, t_cmd *new_node);
-void print_cmds(t_cmd *cmds);
+int			is_builtin(char *cmd_name);
+int			exec_builtin(t_cmd *cmd, t_shell *shell);
+int			ft_echo(char **argv);
+int			ft_cd(char **argv, t_shell *shell);
+int			ft_pwd(void);
+int			ft_env(char **envp);
+int			ft_export(char **argv, t_shell *shell);
+int			ft_unset(char **argv, t_shell *shell);
+int			ft_exit(char **argv, t_shell *shell);
 
-char *get_env_value(char **envp, const char *name);
+int			find_equal_sign(char *arg);
+int			is_name_start(char c);
+int			is_name_char(char c);
+int			is_valid_identifier(char *arg, int eq_index);
+int			has_append_syntax(char *arg, int eq_index);
+int			print_export_no_args(t_shell *shell);
 
-int		handle_redir_only(t_cmd *cmd, t_shell *shell);
-int		handle_builtin(t_cmd *cmd, t_shell *shell);
-void	handle_child(t_cmd *cmd, t_shell *shell, char *path);
-int		handle_parent(int status, t_shell *shell);
+void		expand_cmds(t_cmd *cmds, t_shell *shell);
+char		*expand_one_word(char *word, t_shell *shell);
+char		*expand_heredoc_body_line(char *line, t_shell *shell);
+int			prepare_heredocs(t_cmd *cmds, t_shell *shell);
+int			prepare_heredoc(char *delimiter, int expand_body, t_shell *shell);
+char		*read_line_fd(int fd);
 
-int execute_cmds(t_cmd *cmds, t_shell *shell);
-int execute_single(t_cmd *cmd, t_shell *shell);
-int	execute_pipeline(t_cmd *cmds, t_shell *shell);
-char *resolve_command_path(char *cmd_name, char **envp);
-int ft_strncmp(const char *s1, const char *s2, size_t n);
-char *ft_join_path(char *dir, char *cmd);
-char **ft_split(const char *s, char c);
+char	*read_non_interactive_line(void);
 
-int is_builtin(char *cmd_name);
-int exec_builtin(t_cmd *cmd, t_shell *shell);
-int is_state_builtin(char *cmd_name);
-int env_count(char **envp);
-int env_find_index(char **envp, char *key);
+extern volatile sig_atomic_t	g_signal_status;
 
-int ft_echo(char **argv);
-int ft_cd(char **argv, t_shell *shell);
-int ft_pwd(void);
-int ft_env(char **envp);
-int ft_export(char **argv, t_shell *shell);
-int ft_unset(char **argv, t_shell *shell);
-int ft_exit(char **argv, t_shell *shell);
-
-int apply_redirs(t_redir *redirs);
-void restore_stdio(int stdin_backup, int stdout_backup);
-
-void	exec_child_process(t_cmd *cmd, t_shell *shell, t_pipe_info *info);
-void set_underscore(t_shell *shell, char *path);
-
-void expand_cmds(t_cmd *cmds, t_shell *shell);
-char *expand_one_word(char *word, t_shell *shell);
-char *expand_heredoc_body_line(char *line, t_shell *shell);
-
-int prepare_heredocs(t_cmd *cmds, t_shell *shell);
-int prepare_heredoc(char *delimiter, int expand_body, t_shell *shell);
-char *read_line_fd(int fd);
-extern volatile sig_atomic_t g_signal_status;
-void setup_heredoc_signals(void);
-void sigint_handler(int signum);
-void heredoc_sigint_handler(int signum);
-void set_signal_handler(int signo, void (*handler)(int));
 #endif
