191fbe7c3c86d5c216f4101124725a3c646f1e46
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4a14c10.42berlin.de>
2026-05-28 19:43:33 +0200

    Norminetted

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,41 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkpg-md- <dkpg-md-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/28 17:55:14 by dkpg-md-          #+#    #+#             */
+/*   Updated: 2026/05/28 17:58:16 by dkpg-md-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 static int	is_n_option(char *arg)
 {
-    int i;
+	int	i;
 
-    if (arg == NULL || arg[0] != '-' || arg[1] == '\0')
-        return (0);
-    i = 1;
-    while (arg[i])
-    {
-        if (arg[i] != 'n')
-            return (0);
-        i++;
-    }
-    return (1);
+	if (arg == NULL || arg[0] != '-' || arg[1] == '\0')
+		return (0);
+	i = 1;
+	while (arg[i])
+	{
+		if (arg[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int ft_echo(char **argv)
+int	ft_echo(char **argv)
 {
-    int i;
-    int newline;
+	int	i;
+	int	newline;
 
-    i = 1;
-    newline = 1;
-    while (argv[i] && is_n_option(argv[i]))
-    {
-        newline = 0;
-        i++;
-    }
-    while (argv[i])
-    {
-        printf("%s", argv[i]);
-        if (argv[i + 1])
-            printf(" ");
-        i++;
-    }
-    if (newline)
-        printf("\n");
-    return (0);
+	i = 1;
+	newline = 1;
+	while (argv[i] && is_n_option(argv[i]))
+	{
+		newline = 0;
+		i++;
+	}
+	while (argv[i])
+	{
+		printf("%s", argv[i]);
+		if (argv[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (newline)
+		printf("\n");
+	return (0);
 }
