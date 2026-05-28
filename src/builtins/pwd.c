a232facddeed9298da32b11fc2579cbd1ae36cec
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4a14c10.42berlin.de>
2026-05-28 19:45:12 +0200

    Norminetted

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkpg-md- <dkpg-md-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/28 17:55:23 by dkpg-md-          #+#    #+#             */
+/*   Updated: 2026/05/28 18:01:33 by dkpg-md-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int ft_pwd(void)
+int	ft_pwd(void)
 {
-    char buffer[1024];
+	char	buffer[1024];
 
-    if (getcwd(buffer, sizeof(buffer)) == NULL)
-    {
-        perror("pwd");
-        return (1);
-    }
-    printf("%s\n", buffer);
-    return (0);
+	if (getcwd(buffer, sizeof(buffer)) == NULL)
+	{
+		perror("pwd");
+		return (1);
+	}
+	printf("%s\n", buffer);
+	return (0);
 }
